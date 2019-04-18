a7ae182147aa78404e1b0d6ac23ec2a42ece37f6
HuyLe <lehuydit@gmail.com>
2019-04-18 21:28:17 +0700

    Update

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+// const element = document.getElementById('content');
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// document.body.classList.remove('loading');
